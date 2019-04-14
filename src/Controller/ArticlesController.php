a7a4015b1cd40eb06abc108178ff036466765bff
Matthew Coker <mattacoker@gmail.com>
2019-04-13 20:28:47 -0400

    Routes edited, Articles can now be edited, added, and deleted. Users can also be added, edited, and deleted.

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -12,102 +12,66 @@ use App\Controller\AppController;
  */
 class ArticlesController extends AppController
 {
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|void
-     */
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->loadComponent('Flash'); // Include the FlashComponent
+    }
+
     public function index()
     {
-        $articles = $this->Articles->find('all');
-        $this->set(compact('articles'));
-        /*$articles = $this->paginate($this->Articles);
-
-        $this->set(compact('articles'));*/
+        $this->set('articles', $this->Articles->find('all'));
     }
-    public function view($id = null)
+
+    public function view($id)
     {
         $article = $this->Articles->get($id);
         $this->set(compact('article'));
     }
 
-    /**
-     * View method
-     *
-     * @param string|null $id Article id.
-     * @return \Cake\Http\Response|void
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    /*public function view($id = null)
-    {
-        $article = $this->Articles->get($id, [
-            'contain' => []
-        ]);
-
-        $this->set('article', $article);
-    }*/
-
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
-    /*public function add()
+    public function add()
     {
         $article = $this->Articles->newEntity();
         if ($this->request->is('post')) {
+            // Prior to 3.4.0 $this->request->data() was used.
             $article = $this->Articles->patchEntity($article, $this->request->getData());
             if ($this->Articles->save($article)) {
-                $this->Flash->success(__('The article has been saved.'));
-
+                $this->Flash->success(__('Your article has been saved.'));
                 return $this->redirect(['action' => 'index']);
             }
-            $this->Flash->error(__('The article could not be saved. Please, try again.'));
+            $this->Flash->error(__('Unable to add your article.'));
         }
-        $this->set(compact('article'));
-    }*/
+        $this->set('article', $article);
+    }
 
-    /**
-     * Edit method
-     *
-     * @param string|null $id Article id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    /*public function edit($id = null)
+
+    public function edit($id = null)
     {
-        $article = $this->Articles->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $article = $this->Articles->patchEntity($article, $this->request->getData());
+        $article = $this->Articles->get($id);
+        if ($this->request->is(['post', 'put'])) {
+            // Prior to 3.4.0 $this->request->data() was used.
+            $this->Articles->patchEntity($article, $this->request->getData());
             if ($this->Articles->save($article)) {
-                $this->Flash->success(__('The article has been saved.'));
-
+                $this->Flash->success(__('Your article has been updated.'));
                 return $this->redirect(['action' => 'index']);
             }
-            $this->Flash->error(__('The article could not be saved. Please, try again.'));
+            $this->Flash->error(__('Unable to update your article.'));
         }
-        $this->set(compact('article'));
-    }*/
 
-    /**
-     * Delete method
-     *
-     * @param string|null $id Article id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    /*public function delete($id = null)
+        $this->set('article', $article);
+    }
+
+    // src/Controller/ArticlesController.php
+
+    public function delete($id)
     {
         $this->request->allowMethod(['post', 'delete']);
+
         $article = $this->Articles->get($id);
         if ($this->Articles->delete($article)) {
-            $this->Flash->success(__('The article has been deleted.'));
-        } else {
-            $this->Flash->error(__('The article could not be deleted. Please, try again.'));
+            $this->Flash->success(__('The article with id: {0} has been deleted.', h($id)));
+            return $this->redirect(['action' => 'index']);
         }
-
-        return $this->redirect(['action' => 'index']);
-    }*/
+    }
 }
--- src/Template/Articles/index.ctp
+++ src/Template/Articles/index.ctp
@@ -1,14 +1,16 @@
-<!-- File: src/Template/Articles/index.ctp -->
+<!-- File: src/Template/Articles/index.ctp (delete links added) -->
 
 <h1>Blog articles</h1>
+<p><?= $this->Html->link('Add Article', ['action' => 'add']) ?></p>
 <table>
     <tr>
         <th>Id</th>
         <th>Title</th>
         <th>Created</th>
+        <th>Actions</th>
     </tr>
 
-    <!-- Here is where we iterate through our $articles query object, printing out article info -->
+<!-- Here's where we loop through our $articles query object, printing out article info -->
 
     <?php foreach ($articles as $article): ?>
     <tr>
@@ -19,6 +21,15 @@
         <td>
             <?= $article->created->format(DATE_RFC850) ?>
         </td>
+        <td>
+            <?= $this->Form->postLink(
+                'Delete',
+                ['action' => 'delete', $article->id],
+                ['confirm' => 'Are you sure?'])
+            ?>
+            <?= $this->Html->link('Edit', ['action' => 'edit', $article->id]) ?>
+        </td>
     </tr>
     <?php endforeach; ?>
+
 </table>
